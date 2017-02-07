d561714118d956953eb4b2e7c52ba8d326e26b92
cartzhang <cart033@sina.com>
2017-02-07 15:50:10 +0800

    add optimization to limit the uselese gameobjects
    auto limit the useless number of unusinglinklist

--- Assets/Scripts/PoolManager.cs
+++ Assets/Scripts/PoolManager.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using SLQJ_POOL;
 using UnityEngine.Internal;
+using System.Threading;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Linq;
 
 /// <summary>
 /// 扩展GameObject函数
@@ -61,13 +65,40 @@ namespace SLQJ_POOL
 {
     public class PoolManager : MonoBehaviour
     {
-        private Transform myTransform;
         private static PoolManager instance;
+        private static bool bStartThreadOnce = false;
+        private static bool isStartThread = false;
+        private static Thread tOptimizationThread;
 
         private static List<GameObject> prefabList = new List<GameObject>();
         //存放预制体对应的id，ObjcetPool
         public static Dictionary<int, ObjectPool> poolManagerDic = new Dictionary<int, ObjectPool>();
         private static Dictionary<Transform, ObjectPool> transformDic = new Dictionary<Transform, ObjectPool>();
+        // garbage clean.
+        private static bool clearGarbageFlag = false;
+        private static List<Transform> GarbageList = new List<Transform>(100);
+        private void Update()
+        {
+            CleanGarbageList();
+        }
+
+        private void CleanGarbageList()
+        {
+            if (clearGarbageFlag)
+            {
+                clearGarbageFlag = false;
+                lock (GarbageList)
+                {
+                    Debug.Assert(GarbageList.Count > 0);
+                    Debug.Log("now destroy " + GarbageList.Count + " from pool" + GarbageList[0].name);
+                    for (int i = 0; i < GarbageList.Count; i++)
+                    {
+                        Destroy(GarbageList[i].gameObject);
+                    }
+                    GarbageList.Clear();
+                }
+            }
+        }
 
         //初始化某个预制体对应的对象池
         public static void InitPrefab(GameObject prefab, int initNum = 4)
@@ -91,6 +122,7 @@ namespace SLQJ_POOL
                 return null;
             }
             ObjectPool objPool = GetObjectPool(prefab);
+            StartThreadOnce();
             return objPool.PullObjcetFromPool();
         }
 
@@ -102,6 +134,7 @@ namespace SLQJ_POOL
                 return null;
             }
             ObjectPool objPool = GetObjectPool(prefab, pos, quaternion);
+            StartThreadOnce();
             return objPool.PullObjcetFromPool(pos, quaternion);
         }
 
@@ -189,7 +222,7 @@ namespace SLQJ_POOL
             }
             else
             {
-                GameObject.Destroy(handleTransform.gameObject,delayTime);
+                GameObject.Destroy(handleTransform.gameObject, delayTime);
             }
         }
         //立即回池的接口
@@ -214,5 +247,152 @@ namespace SLQJ_POOL
             Debug.LogError(handleTransform.name + " no find it's ObjectPool");
             return null;
         }
+        // add code to clean pool from time to time.
+        private static void StartThreadOnce()
+        {
+            // start thread to clean pool from time to time.
+            if (!bStartThreadOnce)
+            {
+                bStartThreadOnce = true;
+                ThreadPool.QueueUserWorkItem(AutoToCheckOptimization);
+            }
+        }
+
+        private static void AutoToCheckOptimization(object obj)
+        {
+            Thread.Sleep(10);
+            isStartThread = true;
+            tOptimizationThread = new Thread(OptimizationPool);
+            tOptimizationThread.Start();
+        }
+
+        private static void OptimizationPool()
+        {
+            // check cpu time to start
+            Thread.Sleep(100);
+            // 检测间隔时间20秒
+            float intervalTimeTodetect = 20f;
+            // after how many times counts to reset count.
+            // 循环检测多少次，后记录清零。每次只处理需要处理的前5个池。
+            const int checkTimesForEach = 5;
+            // 临时池管理对象
+            Dictionary<int, ObjectPool> poolManagerTempDic = new Dictionary<int, ObjectPool>();            
+            System.DateTime timeCount = System.DateTime.Now;
+            // 间隔时间内执行一次
+            bool eachMinuteGetDicOnce = false;
+            // 每个池未使用对象超过一半的标记，记录次数
+            Dictionary<int, int> CurrentPoolUnuseCount = new Dictionary<int, int>();
+            // 检测刷新次数，也是一次计数的最大时间。
+            int icountLoopTime = 0;
+            Debug.Log("Thread start");
+            // 休眠时间
+            int sleepTime = 10;
+            while (isStartThread)
+            {
+                Thread.Sleep(sleepTime);
+                if (!eachMinuteGetDicOnce)
+                {
+                    eachMinuteGetDicOnce = true;
+                    poolManagerTempDic = poolManagerDic.ToDictionary(entry => entry.Key,entry => entry.Value);
+                    // loop check 3 time to reset.
+                    if (icountLoopTime % checkTimesForEach == 0)
+                    {
+                        CurrentPoolUnuseCount.Clear();
+                        icountLoopTime = icountLoopTime > 10000000 ? 0 : icountLoopTime;
+                    }
+
+                    // mark unuse nuber for all.
+                    foreach(var element in poolManagerTempDic)
+                    {
+                        ObjectPool opool = element.Value;
+                        int unusinglinkCount = opool.UnusingLinkedList.Count;
+                        // half of all is useless and more than 10.
+                        if (unusinglinkCount * 2 > unusinglinkCount + opool.UsingLinkedList.Count && unusinglinkCount > 10)
+                        {   
+                            MarkCountForUnusingLink(ref CurrentPoolUnuseCount, element.Key);
+                            // satisfy the condition,add unusing link gameobject to garbagelist.
+                            int currentMark = 0;
+                            CurrentPoolUnuseCount.TryGetValue(element.Key,out currentMark);
+                            // be marked three times, add to garbage list.
+                            if (currentMark >= 3)
+                            {
+                                AddObjectsToGarbageList(ref opool.UnusingLinkedList);
+                                // count tick to reset.
+                                CurrentPoolUnuseCount[element.Key] = 0;
+                                clearGarbageFlag = true;
+                                // each time only gathing one pool to process.
+                                break;
+                            }
+                        }
+                    }
+                }
+                // leave time for mainthread to delete gameobjects.
+                if (clearGarbageFlag)
+                {
+                    icountLoopTime = 0;
+                    intervalTimeTodetect = 20f;
+                    Thread.Sleep(1000);
+                    timeCount = System.DateTime.Now;
+                }
+
+                // interval 20 seconds to start check once;
+                if ((System.DateTime.Now - timeCount).TotalSeconds > intervalTimeTodetect)
+                {
+                    timeCount = System.DateTime.Now;
+                    eachMinuteGetDicOnce = false;
+                    poolManagerTempDic.Clear();
+                    icountLoopTime++;
+                    Debug.Log("Loop count is " + icountLoopTime);
+                }
+                // long time nothing happen, expand the detective interval time (max <= 90s).
+                if (icountLoopTime >= 4 )
+                {   
+                    intervalTimeTodetect = intervalTimeTodetect * 2 >= 90f ? 90f : intervalTimeTodetect * 2;
+                    icountLoopTime = 0;
+                    Debug.Log("interval time is " + intervalTimeTodetect);
+                }
+            }
+            return;
+        }
+        
+        private static void MarkCountForUnusingLink(ref Dictionary<int, int> poolUnuseCount,int prefabGuid)
+        {
+            Debug.Assert(null != poolManagerDic);
+            int currentMark = 0;
+            if (poolUnuseCount.ContainsKey(prefabGuid))
+            {
+                poolUnuseCount.TryGetValue(prefabGuid, out currentMark);
+            }
+            currentMark++;
+            if (poolUnuseCount.ContainsKey(prefabGuid))
+            {
+                poolUnuseCount[prefabGuid] = currentMark;
+            }
+            else
+            {
+                poolUnuseCount.Add(prefabGuid, currentMark);
+            }
+        }
+        
+        /// <summary>
+        /// add last gameobject to garbagelist,as when unsing unusinglink is from first place to get.
+        /// </summary>
+        /// <param name="list"></param>
+        private static void AddObjectsToGarbageList(ref LinkedList<Transform> list)
+        {   
+            Debug.Assert(list.Count > 0);            
+            int FlagDestroyNumber = list.Count>>1;
+            for (int i = 0; i < FlagDestroyNumber; i++)
+            {
+                GarbageList.Add(list.Last.Value);
+                list.RemoveLast();
+            }
+        }
+
+        public void Dispose()
+        {
+            isStartThread = false;
+        }
     }
 }
+
---
+++ Assets/Scripts/demo/TestTimeClean.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TestTimeClean : MonoBehaviour
+{
+    [Header("test 预制体")]
+    public GameObject prefab;
+    private List<GameObject> objList;
+    private GameObject obj;
+    // Use this for initialization
+    void Start ()
+    {
+        objList = new List<GameObject>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.M))
+        {
+            obj = gameObject.InstantiateFromPool(prefab);
+            objList.Add(obj);
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            if (objList.Count > 0)
+            {
+                gameObject.DestroyToPool(objList[0]);
+                objList.RemoveAt(0);
+            }
+        }
+    }
+}
+
